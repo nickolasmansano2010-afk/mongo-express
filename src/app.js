bb3163f796fa85f75ef0d1b644d3c070f5980baf
nickolasmansano2010-afk <nickolasmansano2010@gmail.com>
2026-06-01 15:26:44 -0300

    novas atualizacoes

--- src/app.js
+++ src/app.js
@@ -16,25 +16,6 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-app.get("/livros/:id", async (req, res) => {
-    try {
-        const livroEncontrado = await livro.findById(req.params.id);
-        res.status(200).json(livroEncontrado);
-    } catch (erro) {
-        res.status(500).json({ message: erro.message });
-    }
-});
-
-
-app.put("/livros/:id", async (req, res) => {
-    try {
-        const livroAtualizado = await livro.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.status(200).json(livroAtualizado);
-    } catch (erro) {
-        res.status(500).json({ message: erro.message });
-    }
-});
-
 app.delete("/livros/:id", async (req, res) => {
     try {
         await livro.findByIdAndDelete(req.params.id);
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,10 +2,24 @@ import livro from "../models/Livro.js";
 
 class LivroController {
 
-  static async listarLivros (rec, res) {
-    const listaLivros = await livro.find({})
-    res.status(200).json(listaLivros)
+  static async listarLivros (req, res) {
+    try {
+      const listaLivros = await livro.find({})
+      res.status(200).json(listaLivros)
+  } catch (erro) {
+    res.status(500).json({ message: `${erro.message } - falha na requisicao}` });
+  }}
+;
+
+static async listarLivroPorId (req, res) {
+    try {
+      const id = req.params.id;
+        const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado)
+  } catch (erro) {
+    res.status(500).json({ message: `${erro.message } - falha na requisicao do }` });
   }
+}
 
   static async cadastrarLivro (req, res) {
     try {
@@ -15,7 +29,15 @@ class LivroController {
         res.status(500).json({ message: erro.message });
     }
   }
-
+ static async atualizarLivro (req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "livro atualizado com sucesso" });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message } - falha na atualizacao do livro}` });
+    }
+  }
 };
 
 export default LivroController;
