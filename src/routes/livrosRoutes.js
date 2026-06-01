5966d1e8915fd1486e46b7294c046ff2762b6aa7
nickolasmansano2010-afk <nickolasmansano2010@gmail.com>
2026-06-01 14:44:11 -0300

    novas alteracoes

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,6 +1,8 @@
-import { Express } from "express";
+import  Express  from "express";
 import LivroController from "../controllers/livroController.js";
 
 const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
+
+export default routes;
